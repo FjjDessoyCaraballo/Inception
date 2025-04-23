7d9383b0815678997062d6a9453c1766203318a7
Felipe Dessoy Caraballo <63795029+FjjDessoyCaraballo@users.noreply.github.com>
2025-04-23 16:51:28 +0300

    Add files via upload

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mysql_upgrade_db
+mysql_install_db
 
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /etc/mysql/init.sql
-echo "CREATE USER IF NOT EXISTS '$MYQSL_USER'@'%' IDENTIFIED BY '$MYQSL_PASSWORD';" >> /etc/mysql/init.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /etc/mysql/init.sql
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYQSL_USER'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
 echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
 
